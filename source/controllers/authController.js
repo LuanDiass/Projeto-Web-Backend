8fc0199722cb1f32f36617b981bb961ba209b7dd
LuanDiass <luan.diass@hotmail.com>
2021-05-23 11:13:12 -0300

    Correção Letra maiúscula no Require

--- source/controllers/authController.js
+++ source/controllers/authController.js
@@ -29,7 +29,7 @@ router.post("/register", async (req, res) => {
 router.post("/authenticate", async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email }).select("+password");
+  const user = await user.findOne({ email }).select("+password");
 
   if (!user) {
     return res.status(400).send({ error: "Usuario nao encontrado" });
