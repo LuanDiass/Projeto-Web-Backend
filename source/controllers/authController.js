a50e8169bf0527a8e859eab79e5876c6ff284190
miguelnaldi <miguel.naldi@outlook.com>
2021-05-23 02:52:53 -0300

    Token add

--- source/controllers/authController.js
+++ source/controllers/authController.js
@@ -1,13 +1,25 @@
 const express = require("express");
-
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../configuracao/auth");
 const User = require("../models/User");
 
 const router = express.Router();
 
+function generateToken(params = {}) {
+  return jwt.sign(params, authConfig.secret, {
+    expiresIn: 86000,
+  });
+}
+
 router.post("/register", async (req, res) => {
   try {
     const user = await User.create(req.body);
-    return res.send({ user });
+
+    return res.send({
+      user,
+      token: generateToken({ id: user.id }),
+    });
   } catch (err) {
     return res.status(400).send({ error: "Falha no registro" });
   }
@@ -21,6 +33,25 @@ router.post("/authenticate", async (req, res) => {
   if (!user) {
     return res.status(400).send({ error: "Usuario nao encontrado" });
   }
+});
+
+router.post("/authenticate", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return res.status(400).send({ error: "Usuario nao encontrado" });
+  }
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    return res.status(400).send({ error: "Senha invalida" });
+  }
+
+  res.send({
+    user,
+    token: generateToken({ id: user.id }),
+  });
 });
 
 module.exports = (app) => app.use("/auth", router);
