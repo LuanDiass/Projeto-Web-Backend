05533eda513baafdffdf423ed8e85f479147ba6a
LuanDiass <luan.diass@hotmail.com>
2021-05-23 11:12:33 -0300

    Mudança de Rota para Autenticação (Teste)

--- source/controllers/authController.js
+++ source/controllers/authController.js
@@ -25,15 +25,6 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post("/authenticate", async (req, res) => {
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email }).select("+password");
-
-  if (!user) {
-    return res.status(400).send({ error: "Usuario nao encontrado" });
-  }
-});
 
 router.post("/authenticate", async (req, res) => {
   const { email, password } = req.body;
