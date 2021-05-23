b896ccc01f00c65e2fd3891a6327f13e493a9536
miguelnaldi <miguel.naldi@outlook.com>
2021-05-22 22:16:13 -0300

    Revert "Bugs com o register"
    This reverts commit 14424503dfcc34a315ab941511ccddcc657bc0af.

--- source/database/index.js
+++ source/database/index.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/node-project", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-});
+mongoose.connect("mongodb://localhost/noderest", { useMongoClient: true });
+mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
